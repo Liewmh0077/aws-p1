7cb044ddce46ec5afe1134bb4fb9bc7a3b2028b1
Liewmh0077 <67110818+Liewmh0077@users.noreply.github.com>
2023-05-06 22:40:28 +0800

    Add files via upload

---
+++ aws-live-main/config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cmoy300s7jni.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "liewminhui-employee"
+customregion = "us-east-1"
+
